aa4d52d60c38a073701fba270468765e9f75febf
Antoine <dovodjaanto@gmail.com>
2025-09-14 09:07:34 +0200

    I-type output

--- src/print.rs
+++ src/print.rs
@@ -1,4 +1,4 @@
-use crate::{encoder::*, print};
+use crate::encoder::*;
 use colored::*;
 
 fn format_r_type(d: &EncodedInstruction, r: &RType) -> (String, String, String) {
@@ -24,14 +24,42 @@ fn format_r_type(d: &EncodedInstruction, r: &RType) -> (String, String, String) 
     return (instr, bits, hex);
 }
 
-fn format_i_type(d: &EncodedInstruction, i: &IType) {
+fn format_i_type(d: &EncodedInstruction, i: &IType) -> (String, String, String) {
     let operands = &d.operands;
+    let hex = format!("0x{:08x}", i.0).bold().to_string();
+    let instr;
+    if ["lb", "lh", "lw", "lbu", "lhu", "jalr"].contains(&d.mnemonic.as_str()) {
+        instr = format!(
+            "{} {}, {}({})",
+            d.mnemonic.red().bold(),
+            operands[0].green(),
+            operands[1].blue(),
+            operands[2].yellow(),
+        );
+    } else {
+        instr = format!(
+            "{} {}, {}, {}",
+            d.mnemonic.red().bold(),
+            operands[0].green(),
+            operands[1].yellow(),
+            operands[2].blue(),
+        );
+    }
+    let fields = [
+        format!("{:012b}", i.imm()).blue().to_string(),
+        format!("{:05b}", i.rs1()).yellow().to_string(),
+        format!("{:03b}", i.funct3()).red().to_string(),
+        format!("{:05b}", i.rd()).green().to_string(),
+        format!("{:07b}", i.opcode()).red().to_string(),
+    ];
+    let bits = fields.join(" ");
+    return (instr, bits, hex);
 }
 
 pub fn print_encoded_instruction(d: &EncodedInstruction) {
     let out = match &d.instr {
         Instruction::RType(r) => format_r_type(d, r),
-        // Instruction::IType(i) => format_i_type(d, i),
+        Instruction::IType(i) => format_i_type(d, i),
         /* Instruction::IType(_) => {
             print_i_type(d, i);
         }
