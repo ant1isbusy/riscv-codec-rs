0ee9bd6a948c763954e0c6611df9dc6edc1eff7f
Antoine <dovodjaanto@gmail.com>
2025-11-15 18:25:09 +0100

    Refactored confusing variable names

--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,16 @@ pub fn run_cli() {
             .read_line(&mut input)
             .expect("Failed to read line");
 
-        let input = input.trim();
-        if input.eq_ignore_ascii_case("exit")
-            || input.eq_ignore_ascii_case("q")
-            || input.eq_ignore_ascii_case("quit")
+        let clean_input = input.trim();
+        if clean_input.eq_ignore_ascii_case("exit")
+            || clean_input.eq_ignore_ascii_case("q")
+            || clean_input.eq_ignore_ascii_case("quit")
         {
             break;
         }
 
-        if util::is_hex(input) {
-            match u32::from_str_radix(&input[2..], 16) {
+        if util::is_hex(clean_input) {
+            match u32::from_str_radix(&clean_input[2..], 16) {
                 Ok(hex) => match decoder::decode(hex) {
                     Ok(instr) => {
                         format::print_encoded_instruction(&instr);
@@ -36,7 +36,7 @@ pub fn run_cli() {
                 Err(_) => println!("Error parsing hex input:"),
             }
         } else {
-            match encoder::encode(input) {
+            match encoder::encode(clean_input) {
                 Ok(d) => format::print_encoded_instruction(&d),
                 Err(e) => println!("Error encoding instruction: {:?}", e),
             }
